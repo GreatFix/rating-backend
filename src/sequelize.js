08b4442d493ac586ffb114a7be4fb742c5439b3a
У М <umalat.magma@gmail.com>
2021-04-09 02:29:10 +0300

    Изменил связи

--- src/sequelize.js
+++ src/sequelize.js
@@ -2,29 +2,31 @@ const Sequelize = require("sequelize");
 const UserModel = require("./models/User");
 const TargetModel = require("./models/Target");
 const FeedbackModel = require("./models/Feedback");
-const ResponseModel = require("./models/Response");
+const CommentModel = require("./models/Comment");
+const CommentListModel = require("./models/CommentList");
 
-const sequelize = new Sequelize("rating-bd", "postgres", "1234", {
-  host: "localhost",
-  dialect: "postgres",
-  // dialectOptions: {
-  //   ssl: {
-  //     rejectUnauthorized: false,
-  //   },
-  // },
-});
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 const User = UserModel(sequelize, Sequelize);
 const Target = TargetModel(sequelize, Sequelize);
 const Feedback = FeedbackModel(sequelize, Sequelize);
-const Response = ResponseModel(sequelize, Sequelize);
+const Comment = CommentModel(sequelize, Sequelize);
+const CommentList = CommentListModel(sequelize, Sequelize);
 
 User.hasMany(Feedback, { onDelete: "cascade" });
 Feedback.belongsTo(User);
+
 Target.hasMany(Feedback, { onDelete: "cascade" });
 Feedback.belongsTo(Target);
-Response.hasOne(Feedback);
-Feedback.belongsTo(Response, { onDelete: "cascade" });
+
+User.hasMany(Comment, { onDelete: "cascade" });
+Comment.belongsTo(User);
+
+CommentList.hasOne(Feedback, { onDelete: "cascade" });
+Feedback.belongsTo(CommentList);
+
+CommentList.hasMany(Comment, { onDelete: "cascade" });
+Comment.belongsTo(CommentList);
 
 sequelize
   .authenticate()
@@ -32,8 +34,8 @@ sequelize
   .catch((err) => console.error("Connection error:", err));
 
 sequelize
-  .sync({ after: true })
+  .sync()
   .then(() => console.log("Synchronized"))
   .catch((err) => console.error("Synchronization error:" + err));
 
-module.exports = { User, Target, Feedback, Response };
+module.exports = { User, Target, Feedback, Comment, CommentList };
